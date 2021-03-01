e485d042a6e949ec3d1318b3f089975e767447d4
Brandon Ward <brandonward.gk@gmail.com>
2021-03-01 11:49:35 -0700

    add psycopg2, update app database to use postgress and env variable for DB

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
